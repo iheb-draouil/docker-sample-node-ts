de0628b55ffc223d8bc795886a3b0c20a944462b
iheb draouil <iheb.draouil@outlook.com>
2022-06-05 15:35:49 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
 import http from 'http';
 
 const server = http.createServer((request, response) => {
-
-    let data: string = '';
     
-    request.on('data', chunk => {
-        data += chunk;
-    });
+    const fullUrl = new URL('http://domain.com' + request.url);
 
-    request.on('end', () => {
-        const parsedData = JSON.parse(data);
-        response.write(`Your name is '${parsedData.name}'!`);
-        response.end();
-    });
+    const name = fullUrl.searchParams.get('name');
 
+    if (name) {
+        response.write(`Your name is '${name}'!`);
+    }
+    
+    response.end();
 });
 
 server.listen(8080);
